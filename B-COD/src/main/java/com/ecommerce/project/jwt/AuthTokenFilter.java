f54152d5b556efbfd55e5f08622982a665ad3af9
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-01 21:50:35 +0700

    Make AuthTokenFilter class for Jwt purpose

---
+++ B-COD/src/main/java/com/ecommerce/project/jwt/AuthTokenFilter.java
@@ -0,0 +1,22 @@
+package com.ecommerce.project.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
