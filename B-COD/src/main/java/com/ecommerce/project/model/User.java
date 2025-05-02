08661903d634da017f96d17002f867df1c2399a4
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-02 23:12:40 +0700

    Make a Many to many relationship between user and roles

--- B-COD/src/main/java/com/ecommerce/project/model/User.java
+++ B-COD/src/main/java/com/ecommerce/project/model/User.java
@@ -5,7 +5,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -35,4 +40,15 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    @Getter
+    @Setter
+    @ManyToMany(cascade = {CascadeType.PERSIST , CascadeType.MERGE},
+                fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
 }
