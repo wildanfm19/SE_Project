0415db6fca67f03429f2370d8500a70dc914a723
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-04 19:05:48 +0700

    Added seller side in the User and Product Entity

--- B-COD/src/main/java/com/ecommerce/project/model/User.java
+++ B-COD/src/main/java/com/ecommerce/project/model/User.java
@@ -58,5 +58,10 @@ public class User {
                 inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+    @OneToMany(mappedBy = "user" , cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                orphanRemoval = true)
+    private Set<Product> products;
+
+
 
 }
