f4411282c4d3c7024b42912c5b2ad3730e3a034b
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-05 22:04:42 +0700

    Added signin endpoint in AuthController and LoginRequest also UserInfoResponse class

---
+++ B-COD/src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -0,0 +1,20 @@
+package com.ecommerce.project.security.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoResponse {
+
+    private Long id;
+    private String username;
+    private List<String> roles;
+    private String jwtToken;
+
+
+}
