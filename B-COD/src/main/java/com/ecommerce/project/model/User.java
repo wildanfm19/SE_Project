34caf410d69c7a3917af8d16c9cb3eabfbee809b
wildanfm19 <dracko.maaruftm@gmail.com>
2025-05-06 20:11:39 +0700

    Make new field and maintaining new relationship between User , Cart and Product

--- B-COD/src/main/java/com/ecommerce/project/model/User.java
+++ B-COD/src/main/java/com/ecommerce/project/model/User.java
@@ -66,9 +66,16 @@ public class User {
     private List<Address> addresses = new ArrayList<>();
 
     @ToString.Exclude
+    @OneToOne(mappedBy = "user" , cascade = {CascadeType.PERSIST , CascadeType.MERGE} , orphanRemoval = true)
+    private Cart cart;
+
+
+    @ToString.Exclude
     @OneToMany(mappedBy = "user" , cascade = {CascadeType.PERSIST, CascadeType.MERGE},
                 orphanRemoval = true)
     private Set<Product> products;
+
+
 
 
 
